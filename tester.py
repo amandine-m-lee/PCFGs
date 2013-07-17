6b6be88ffd69c627fb1706d388055c66a7cdc4be
Amandine Lee <amandine.m.lee@gmail.com>
2013-07-17 18:19:26 -0400

    All the new outfiles

--- tester.py
+++ tester.py
@@ -3,9 +3,13 @@ from prob_generator import *
 pg = ProbGen("new.counts")
 
 assert pg.nonterm_counts['ADJP'] == 81
-assert pg.unary_counts['NOUN']['years'] == 5 
+assert pg.unary_counts['years']['NOUN'] == 5 
 assert pg.binary_counts['SQ'][('VERB', 'NP+DET')] == 8
 
 assert pg.nonterm_counts['NP+ADVP'] == 1
 
-print sum([pg.unary_counts[d]['_RARE_'] for d in pg.unary_counts.keys()])
+tags = pg.unary_counts['a'].keys()
+
+for tag in tags:
+    print tag, pg.emm_prob(tag, 'a')
+    print '_RARE_', pg.emm_prob(tag, '_RARE_')
